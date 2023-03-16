157f47c8cf2e3abebff477be4c5bd9dcad1a8172
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-15 21:41:03 -0400

    added set get for names

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,32 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    ArrayList<Student> studentList = new ArrayList<>();
+    ArrayList<Double> examScores = new ArrayList<Double>();
+
+    public Student(String first, String last, Double[] testScores) {
+        this.firstName = first;
+        this.lastName = last;
+        Collections.addAll(examScores, testScores);
+    }
+    public void setFirstName(String first){
+        this.firstName = first;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setLastName(String last) {
+        this.lastName = last;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    
+
 }
